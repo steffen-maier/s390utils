6ab82307f549c097f5c24a2e6c91408d88b4cf18
Bill Nottingham <notting@redhat.com>
2011-02-23 16:55:16 -0500

    Retire s390utils.

--- s390.csh
+++
@@ -1,5 +0,0 @@
-# /etc/profile.d/s390.csh - set TERM variable
-
-if ( `/sbin/consoletype stdout` == "serial" ) then
-    setenv TERM dumb
-endif
--- s390.sh
+++
@@ -1,6 +0,0 @@
-# /etc/profile.d/s390.sh - set TERM variable
-
-contype=`/sbin/consoletype stdout`
-if [ "$contype" == "serial" ]; then
-    export TERM=dumb
-fi
