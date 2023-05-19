steffen-maier/s390utils

commit 9e98b44d8b9b53c040a8219df970a5a359d0260a

- add post GA fixes

s390utils.spec
@@ -10,7 +10,7 @@
 Name:           s390utils
 Summary:        Utilities and daemons for IBM z Systems
 Version:        2.25.0
-Release:        3%{?dist}
+Release:        4%{?dist}
 Epoch:          2
 License:        MIT
 ExclusiveArch:  s390 s390x
@@ -38,7 +38,7 @@ Patch0:         s390-tools-zipl-invert-script-options.patch
 Patch1:         s390-tools-zipl-blscfg-rpm-nvr-sort.patch
 
 # upstream fixes/updates
-#Patch100:       s390utils-%%{version}-fedora.patch
+Patch100:       s390utils-%%{version}-fedora.patch
 
 Requires:       s390utils-core = %{epoch}:%{version}-%{release}
 Requires:       s390utils-base = %{epoch}:%{version}-%{release}
@@ -67,7 +67,7 @@ be used together with the zSeries (s390) Linux kernel and device drivers.
 %patch1 -p1 -b .blscfg-rpm-nvr-sort
 
 # upstream fixes/updates
-#%%patch100 -p1
+%patch100 -p1
 
 # drop -Werror from genprotimg to allow building with GCC 12
 sed -i.bak -e 's/-Werror//g' genprotimg/src/Makefile genprotimg/boot/Makefile
@@ -907,6 +907,9 @@ User-space development files for the s390/s390x architecture.
 
 
 %changelog
+* Tue Jan 31 2023 Dan Horák <dan[at]danny.cz> - 2:2.25.0-4
+- add post GA fixes
+
 * Mon Jan 23 2023 Dan Horák <dan[at]danny.cz> - 2:2.25.0-3
 - revert the kernel install script change
 

s390utils-2.25.0-fedora.patch
@@ -1,0 +1,77 @@
+From d721eb018973842a432869f6f9efd5752b2e7f19 Mon Sep 17 00:00:00 2001
+From: Marc Hartmayer <mhartmay@linux.ibm.com>
+Date: Mon, 19 Dec 2022 09:51:51 +0000
+Subject: [PATCH 1/2] zgetdump/Makefile: don't use `.check_dep_zgetdump` as
+ linker input
+
+The `.check_dep_zgetdump` file is used to cache the result of the
+dependency checks and should not be used as input for linking or
+anything else. Let's add it as dependency for the objects file. This
+shouldn't cause any problems since the Makefile rule for object files is
+defined in `common.mak` as follows:
+
+%.o: %.c
+	$(CC) $(ALL_CPPFLAGS) $(ALL_CFLAGS) -c $< -o $@
+
+Fixes: https://github.com/ibm-s390-linux/s390-tools/issues/147
+Fixes: 8d8d5e9746a4 ("zdump: Fix Makefile dependencies")
+Signed-off-by: Marc Hartmayer <mhartmay@linux.ibm.com>
+Reviewed-by: Jan Hoeppner <hoeppner@linux.ibm.com>
+Signed-off-by: Jan Hoeppner <hoeppner@linux.ibm.com>
+---
+ zdump/Makefile | 4 +++-
+ 1 file changed, 3 insertions(+), 1 deletion(-)
+
+diff --git a/zdump/Makefile b/zdump/Makefile
+index ca8aadc..934dc47 100644
+--- a/zdump/Makefile
++++ b/zdump/Makefile
+@@ -119,7 +119,9 @@ libs = $(rootdir)/libutil/libutil.a $(LIBPV)
+ 
+ all: $(BUILD_TARGETS)
+ 
+-zgetdump: .check_dep_zgetdump $(OBJECTS) $(libs)
++$(OBJECTS): .check_dep_zgetdump
++
++zgetdump: $(OBJECTS) $(libs)
+ 
+ skip-zgetdump:
+ 	echo "  SKIP    zgetdump due to unresolved dependencies"
+-- 
+2.39.1
+
+
+From d9f54f76e2e6f17e0b5bafdc0d09ff0e72edd474 Mon Sep 17 00:00:00 2001
+From: Ingo Franzki <ifranzki@linux.ibm.com>
+Date: Fri, 20 Jan 2023 11:04:18 +0100
+Subject: [PATCH 2/2] zkey: Support EP11 host library version 4
+MIME-Version: 1.0
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
+
+Try to load libep11.so.4 if available, but fallback to older
+library versions if not.
+
+Reviewed-by: Jörg Schmidbauer <jschmidb@de.ibm.com>
+Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>
+Signed-off-by: Steffen Eiden <seiden@linux.ibm.com>
+---
+ zkey/ep11.c | 2 +-
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+
+diff --git a/zkey/ep11.c b/zkey/ep11.c
+index 58dc3c5..8359929 100644
+--- a/zkey/ep11.c
++++ b/zkey/ep11.c
+@@ -35,7 +35,7 @@
+  * Definitions for the EP11 library
+  */
+ #define EP11_LIBRARY_NAME	"libep11.so"
+-#define EP11_LIBRARY_VERSION	3
++#define EP11_LIBRARY_VERSION	4
+ #define EP11_WEB_PAGE		"http://www.ibm.com/security/cryptocards"
+ 
+ /**
+-- 
+2.39.1
+

commit a6e30a308a910c5fc82590a0f44f18ea86c77998

Unescape Patch100 definition
Broken with commit 9e98b44d8b9b53c040a8219df970a5a359d0260a.

s390utils.spec
@@ -38,7 +38,7 @@ Patch0:         s390-tools-zipl-invert-script-options.patch
 Patch1:         s390-tools-zipl-blscfg-rpm-nvr-sort.patch
 
 # upstream fixes/updates
-Patch100:       s390utils-%%{version}-fedora.patch
+Patch100:       s390utils-%{version}-fedora.patch
 
 Requires:       s390utils-core = %{epoch}:%{version}-%{release}
 Requires:       s390utils-base = %{epoch}:%{version}-%{release}